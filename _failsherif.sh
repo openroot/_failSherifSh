9fb735f56a801d5936d98a6bd6ad531beec0a73d
Debaprasad Tapader <dev.openroot@gmail.com>
2022-10-10 13:05:43 +0530

    first commit

---
+++ _failsherif.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Name: _failsherif.sh
+# Purpose: failsherif bash script
+# -------------------------------
+
+# region welcome
+
+echo "_failsherif"
+echo "༂࿐~ +strange easy world"
+echo
+
+# region end
+
+# region function
+
+
+# region end
+
+# region testing
+
+
+# region end
+
+# region input
+
+
+# region end
+
+# region execute
+
+
+# region end
+
+# region report
+
+
+# region end
+
+# region exit
+
+echo
+echo ",say enter any string to exit: "
+read dump
+echo "$dump"
+
+# region end
