bd0d4e6a4dfb0418d58263489d27db962692030f
openroot <dev.openroot@gmail.com>
2022-10-14 00:06:30 +0530

    temp progress

--- _failsherif.sh
+++ _failsherif.sh
@@ -19,6 +19,17 @@ echo
 
 # region function
 
+fs_autooperation_auto=0 # default auto-operation is disabled
+fs_autooperation_delayinsec=1 # default value in seconds(s)
+# function to configure auto-operation
+function fs.autooperation () {
+	if [ $fs_autooperation_auto == 0 ]; then 	# entering in auto-operation first time
+		fs_autooperation_auto=1
+		echo
+		read -rp "Enter delay in second(s) for auto operation: " "fs_autooperation_delayinsec"
+	fi
+}
+
 function fs.samplefunction () {
 	local value1=100.12
 	local value2=3
@@ -30,8 +41,27 @@ function fs.samplefunction () {
 function fs.codetestfunction () {
 	# attested start
 	echo ",functional area an to test sh codes"
-	
 	# attested end
+}
+
+fs_restapiinvoke_lastresult=""
+function fs.restapiinvoke () {
+	if [ "$fs_restapiinvoke_lastresult" != "" ]; then
+		clear
+		echo "Response from server: "
+		echo "$fs_restapiinvoke_lastresult"
+		echo
+	fi
+
+	#result=$(curl --silent GET --header "Accept: */*" "https://corona-virus-stats.herokuapp.com/api/v1/cases/general-stats")
+	#result=$(curl -# GET --header "Accept: */*" "https://corona-virus-stats.herokuapp.com/api/v1/cases/general-stats")
+	result=$(curl -X GET --header "Accept: */*" "https://corona-virus-stats.herokuapp.com/api/v1/cases/general-stats")
+
+	clear
+	echo "Response from server: "
+	echo $result
+
+	fs_restapiinvoke_lastresult=$result
 }
 
 # region end
@@ -46,27 +76,43 @@ echo
 # region execute
 
 while : ; do
-	read -rp "Please enter option for operation [1|a/2|b/3|c/n|N]: " "operation"
-	echo "Entered option = $operation."
-	echo
+	if [ $fs_autooperation_auto == 0 ]; then
+		read -rp "Please enter option for operation [a|A/b|B/c|C/n|no]: " "operation"
+		echo "Entered option = $operation."
+		echo
+	else
+		sleep $fs_autooperation_delayinsec
+		#clear
+	fi
 
     case $operation in
 
-	1 | a | "A")
+	a)
 		fs.samplefunction
 	;;
+	A)
+		fs.samplefunction
+		fs.autooperation
+	;;
 
-	2 | b | "B")
+	b)
 		fs.codetestfunction
 	;;
-
-	3 | c | "C")
-		echo "Option 3rd entered."
+	B)
+		fs.codetestfunction
+		fs.autooperation
 	;;
 
-	n | "N")
-		# on selection exit the app
-		break
+	c)
+		fs.restapiinvoke
+	;;
+	C)
+		fs.restapiinvoke
+		fs.autooperation
+	;;
+
+	n | "no")
+		break # on selection exit the app
 	;;
 
 	*)
@@ -88,5 +134,6 @@ done
 # region exit
 
 echo "App executed successfully."
+exit
 
 # region end
